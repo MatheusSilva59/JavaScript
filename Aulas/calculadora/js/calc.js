2de381ee943fed77e6344a25078cb1a64350b3e7
MatheusSilva59 <matheusdasilvandk@gmail.com>
2022-07-25 12:46:48 -0300

    Update calc.js

--- Aulas/calculadora/js/calc.js
+++ Aulas/calculadora/js/calc.js
@@ -98,7 +98,7 @@ function saveNumber(operation){
     if (operation == 'changeSign'){
         var tempNumber = changeSign(element())
         if (end === true){lastNumber = tempNumber}
-        return document.getElementById('numDisplay').value = tempNumber
+        return document.getElementById('numDisplay').value = tempNumber.toString().replace('.', ',')
     }
 
     var sign = whichOperation(operation)
@@ -183,7 +183,7 @@ function equal(endLocal){
     }
 
     lastNumber = result
-    document.getElementById('numDisplay').value = result.toString().replace('.', decimalPlace).slice(0, 15)
+    document.getElementById('numDisplay').value = result.toString().replace('.', decimalPlace).slice(0, 14)
     result = 0
     newValue = false
     newOperation = false
