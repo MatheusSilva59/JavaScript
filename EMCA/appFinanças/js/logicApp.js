2d51a06ef2e8db4f0d7e37881f9f5b64311f4b4b
MatheusSilva59 <matheusdasilvandk@gmail.com>
2022-08-25 12:30:18 -0300

    att

--- EMCA/appFinanças/js/logicApp.js
+++ EMCA/appFinanças/js/logicApp.js
@@ -1,15 +1,20 @@
-
 function add() {
     if (isValid()) {
-        window.localStorage.setItem(localStorage.length, JSON.stringify(new Expense()))
-        myModal.show()
+        window.localStorage.setItem(realLengthStorage(), JSON.stringify(new Expense()))
+        myModalSuccess.show()
     }
-    else {
-        //console.log(localStorage)
-
+    else{
+        myModalFail.show()
     }
 }
-
+function realLengthStorage() {
+    let nextIndex = 0
+    for (let i = 0; i < localStorage.length; i++) {
+        nextIndex = nextIndex < localStorage.key(i) ? localStorage.key(i) : nextIndex
+    }
+    nextIndex = Number(nextIndex) + 1
+    return nextIndex
+}
 class Expense {
     constructor() {
         this.date = document.getElementById('date').value
@@ -47,42 +52,94 @@ function isValid() {
     }
 }
 let filterClass
-function addClassToFilter(){
+let findForIdentity
+let findForProximity
+let filterForProximity = true
+
+function initiate() {
+    findForIdentity = document.getElementById('findForIdentity')
+    findForProximity = document.getElementById('findForProximity')
+
+    findForIdentity.addEventListener('click', () => {
+        filterForProximity = false
+        findForIdentity.className = 'dropdown-item active'
+        findForProximity.className = 'dropdown-item'
+    })
+    findForProximity.addEventListener('click', () => {
+        filterForProximity = true
+        findForProximity.className = 'dropdown-item active'
+        findForIdentity.className = 'dropdown-item'
+    })
+
     filterClass = new Expense()
     jsonToObject()
+    showAllExpense()
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 }
-function jsonToObject(){
-    for (let item = 0; item < localStorage.length; item++){
+function jsonToObject() {
+    for (let item = 0; item < localStorage.length; item++) {
         window['class' + item] = JSON.parse(localStorage[localStorage.key(item)])
     }
 }
 function showAllExpense() {
     var tbody = document.querySelector('#tbody-main')
     //clear all nodes
-    while (tbody.firstChild){
+    while (tbody.firstChild) {
         tbody.removeChild(tbody.firstChild)
     }
 
     for (let item = 0; item < localStorage.length; item++) {
         let tr = document.createElement('tr')
-        for (index in window ['class' + item]) {
+        for (index in window['class' + item]) {
             let td = document.createElement('td')
-            td.textContent = window['class' + item][index]
+            if (window['class' + item][index].length > 20) {
+                console.log('grande')
+                td.textContent = window['class' + item][index].toString().slice(0, 10)
+                let btn = document.createElement('button')
+                let i = document.createElement('i')
+
+                //btn popover
+                btn.className = 'd-flex btn-popover'
+                btn.id = 'btn' + localStorage.key(item)
+                i.className = 'fa-solid fa-ellipsis align-self-end icon-popover'
+                btn.appendChild(i)
+
+                btn.addEventListener('click', () => { console.log('ampliar') })
+                btn.type = 'button'
+
+                btn.setAttribute('data-bs-container', 'body')
+                btn.setAttribute('data-bs-toggle', 'popover')
+                btn.setAttribute('data-bs-placement', 'bottom')
+                btn.setAttribute('tabindex', '0')
+                btn.setAttribute('role', 'button')
+                btn.setAttribute('data-bs-trigger', 'focus')
+                btn.setAttribute('data-bs-content', window['class' + item][index])
+                //btn popover
+                td.appendChild(btn)
+            }
+            else {
+                if (index === 'category') {
+                    td.textContent = translateCategory(window['class' + item][index])
+                }
+                else {
+                    td.textContent = window['class' + item][index]
+                }
+            }
             tr.appendChild(td)
         }
         // button
         let td = document.createElement('td')
-        let btn = document.createElement('btn')
+        let btn = document.createElement('button')
         let i = document.createElement('i')
-        td.className = 'text-center'  
+        td.className = 'text-center'
         btn.className = 'btn btn-danger'
         i.className = 'fa-solid fa-xmark'
         btn.appendChild(i)
         btn.addEventListener('click', () => {
             let tempTr = document.getElementById(`tr${localStorage.key(item)}`)
             tbody.removeChild(tempTr)
-            //window.localStorage.removeItem(localStorage.key(item))
-            reOrderStorage()
+            window.localStorage.removeItem(localStorage.key(item))
         })
         td.appendChild(btn)
         tr.appendChild(td)
@@ -91,35 +148,55 @@ function showAllExpense() {
         tbody.appendChild(tr)
     }
 }
-function updateValue(){
+function translateCategory(value) {
+    switch (value) {
+        case 'education':
+            return 'Educação'
+        case 'food':
+            return 'Alimentação'
+        case 'transport':
+            return 'Transporte'
+        case 'leisure':
+            return 'Lazer'
+        case 'health':
+            return 'Saúde'
+    }
+}
+function updateValue() {
     filterClass.date = document.getElementById('date').value
     filterClass.category = document.getElementById('category').value
     filterClass.description = document.getElementById('description').value
     filterClass.cashValue = document.getElementById('cash-value').value
 }
-function filter(){
+function filter() {
     updateValue()
-    for (let item = 0; item < localStorage.length; item++){
+    for (let item = 0; item < localStorage.length; item++) {
         let tempTr = document.getElementById(`tr${localStorage.key(item)}`)
-        for (index in window ['class' + item]) {
-            if (index != 'description'){
-                if (filterClass[index] === window['class' + item][index] || filterClass[index] === '' || filterClass[index] === 'Tipo'){
+        for (index in window['class' + item]) {
+            if (index != 'description' || !filterForProximity) {
+                //identity filter
+                if (filterClass[index] === window['class' + item][index] || filterClass[index] === '' || filterClass[index] === 'Tipo') {
                     tempTr.style.display = 'table-row'
                 }
-                else{
+                else {
                     tempTr.style.display = 'none'
                     break
                 }
             }
+            else {
+                let flterTempTrim = filterClass[index].toString().trim()
+                if (flterTempTrim != '') {
+                    let tempRegExp = new RegExp(`([${window['class' + item][index]}]{${filterClass[index].length}})`, 'i')
+                    if (filterClass[index].match(tempRegExp)) {
+                        tempTr.style.display = 'table-row'
+                    }
+                    else {
+                        tempTr.style.display = 'none'
+                        break
+                    }
+                }
+            }
         }
-    }
-}
-function reOrderStorage(){
-    for (let item = 0; item < localStorage.length; item++){
-        localStorage.setItem(item, JSON.stringify(window ['class' + localStorage.key(item)]))
-        console.log(window ['class' + localStorage.key(item)])
-        console.log(localStorage.key(item))
-        console.log(item)
     }
 }
 
